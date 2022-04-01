d5b24d083a6c7dcc647dbe166a2dee7a40e928bb
Rastko Mitrovic <rastkomitrovic@gmail.com>
2022-04-01 21:34:09 +0200

    izmenjen upit

--- src/main/java/com/fon/konstrukcije/microservice/orders/repository/NarudzbeniceRepository.java
+++ src/main/java/com/fon/konstrukcije/microservice/orders/repository/NarudzbeniceRepository.java
@@ -11,11 +11,11 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface NarudzbeniceRepository extends PagingAndSortingRepository<Narudzbenica, Integer> {
 	
-	@Query("SELECT n FROM Narudzbenica n JOIN FETCH n.klijent AS k WHERE n.brojNarudzbenice=:search OR concat(k.ime, ' ', k.prezime) like '%:search' OR n.ukupno=:search OR function('dateFormat', n.datumKreiranja, '%Y-%m-%d %T') LIKE '%search%' OR (n.datumAzuririranja IS NOT NULL AND function('dateFormat', n.datumAzuriranja, '%Y-%m-%d %T') LIKE '%:search%')")
+	@Query(value = "SELECT n FROM Narudzbenica n JOIN FETCH n.klijent AS k WHERE n.brojNarudzbenice=:search OR concat(k.ime, ' ', k.prezime) like concat('%',:search,'%') OR n.ukupno=:search OR DATE_FORMAT(n.datumKreiranja, '%Y-%m-%d %T') LIKE concat('%',:search,'%') OR (n.datumAzuriranja IS NOT NULL AND DATE_FORMAT(n.datumAzuriranja, '%Y-%m-%d %T') LIKE concat('%',:search,'%'))", nativeQuery = true)
 	Page<Narudzbenica> findAllBySearch(Pageable paging, String search);
 	
 	@Override
-	@Query("SELECT n from Narudzbenica n join fetch n.klijent join fetch n.stavkeNarudzbenice WHERE n.brojNarudzbenice =:id")
+	@Query("SELECT n from Narudzbenica n join fetch n.klijent join fetch n.stavkeNarudzbenice st join fetch st.proizvod WHERE n.brojNarudzbenice =:id")
 	Optional<Narudzbenica> findById(Integer id);
 }
  
