e8839c307a03644eab7b1ca51875968684a4b67c
Rastko Mitrovic <rastkomitrovic@gmail.com>
2022-04-01 17:24:49 +0200

    Pomeren NarudzbeniceRepository u repository paket

--- src/main/java/com/fon/konstrukcije/microservice/orders/repository/NarudzbeniceRepository.java
+++ src/main/java/com/fon/konstrukcije/microservice/orders/repository/NarudzbeniceRepository.java
@@ -1,4 +1,4 @@
-package com.fon.konstrukcije.microservice.orders;
+package com.fon.konstrukcije.microservice.orders.repository;
 
 import com.fon.konstrukcije.microservice.orders.entity.Narudzbenica;
 import org.springframework.data.repository.PagingAndSortingRepository;
